6b45e44001cbcac107e8e273f1accbffceccf1eb
konstserg <konst-serg@mail.ru>
2022-05-29 22:50:22 +0700

    ls_1

---
+++ mainapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+
+def contact(request):
+    return render(request, 'mainapp/contact.html')
+
+
+'''def index(request):
+   return render(request, 'mainapp/index.html')'''
